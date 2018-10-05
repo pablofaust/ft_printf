5f33263e3a7bc1e6dc2c8a676f7b729f3b09c428
Pablo FAUST <pfaust@e3r5p5.42.fr>
2018-10-05 12:19:58 +0200

    premiers essais avec stdarg

--- tests/main.c
+++ tests/main.c
@@ -6,73 +6,78 @@ int			main()
 	//
 	// test un seul caractere //
 	//
-	ft_printf(" ");	
-	ft_printf("a");
+//	ft_printf(" ");	
+//	ft_printf("a");
 	//
 	// test plusieurs caracteres//
 	//
-	ft_printf("    ");	
-	ft_printf("abc");
-	ft_printf("abc abc ");	
-	ft_printf("abc\n");
+//	ft_printf("    ");	
+//	ft_printf("abc");
+//	ft_printf("abc abc ");	
+//	ft_printf("abc\n");
 	//
 	// test conversion simple //
 	// 
-	ft_printf("%s");	
-	ft_printf("%S");	
-	ft_printf("%p");	
-	ft_printf("%d");	
-	ft_printf("%D");	
-	ft_printf("%i");	
-	ft_printf("%o");	
-	ft_printf("%O");	
-	ft_printf("%u");	
-	ft_printf("%U");	
-	ft_printf("%x");	
-	ft_printf("%X");	
-	ft_printf("%c");	
-	ft_printf("%C");
+//	ft_printf("%s");	
+//	ft_printf("%S");	
+//	ft_printf("%p");	
+//	ft_printf("%d");	
+//	ft_printf("%D");	
+//	ft_printf("%i");	
+//	ft_printf("%o");	
+//	ft_printf("%O");	
+//	ft_printf("%u");	
+//	ft_printf("%U");	
+//	ft_printf("%x");	
+//	ft_printf("%X");	
+//	ft_printf("%c");	
+//	ft_printf("%C");
 	// test attributs //
 	//
-	ft_printf("%#c");
-	ft_printf("%0c");
-	ft_printf("%-c");
-	ft_printf("%+c");
-	ft_printf("% c");
-	ft_printf("%#0-+ c");
+//	ft_printf("%#c");
+//	ft_printf("%0c");
+//	ft_printf("%-c");
+//	ft_printf("%+c");
+//	ft_printf("% c");
+//	ft_printf("%#0-+ c");
 	// 
 	// test largeur //
 	//
-	ft_printf("%3c");
-	ft_printf("%31c");
-	ft_printf("%#31c");
-	ft_printf("%#0-+ 31c");
+//	ft_printf("%3c");
+//	ft_printf("%31c");
+//	ft_printf("%#31c");
+//	ft_printf("%#0-+ 31c");
 	//
 	// test precision //
 	//
-	ft_printf("%.5c");
-	ft_printf("%.55c");
-	ft_printf("%31.55c");
-	ft_printf("%0.55c");
-	ft_printf("%#0-+ 31.55c");
+//	ft_printf("%.5c");
+//	ft_printf("%.55c");
+//	ft_printf("%31.55c");
+//	ft_printf("%0.55c");
+//	ft_printf("%#0-+ 31.55c");
 	//
 	// test modificateur //
 	//
-	ft_printf("%hhc");
-	ft_printf("%hc");
-	ft_printf("%lc");
-	ft_printf("%llc");
-	ft_printf("%jc");
-	ft_printf("%zc");
-	ft_printf("%#hc");
-	ft_printf("%31hc");
-	ft_printf("%.5hc");
-	ft_printf("%#0-+ 31.55hc");
+//	ft_printf("%hhc");
+//	ft_printf("%hc");
+//	ft_printf("%lc");
+//	ft_printf("%llc");
+//	ft_printf("%jc");
+//	ft_printf("%zc");
+//	ft_printf("%#hc");
+//	ft_printf("%31hc");
+//	ft_printf("%.5hc");
+//	ft_printf("%#0-+ 31.55hc");
 	//
-	// chaine + conversion //
+	// test chaine + conversion //
 	//
-	ft_printf("nombre = %d");
-	ft_printf("nombre = %d, caractere = %c\n");
-	ft_printf("nombre = %#034.54ld, caractere = %+- 23.54hhs\n");
+//	ft_printf("nombre = %d");
+//	ft_printf("nombre = %d, caractere = %c\n");
+//	ft_printf("nombre = %#034.54ld, caractere = %+- 23.54hhs\n");
+//	
+	// test un argument //
+	//
+	ft_printf("%d\n", 5);
+	//
 	return (0);
 }
